f3bef35ec20f2ac90d3cfb5613efb7863fb1fb9c
oniszczak <aleks42@gmail.com>
2023-01-09 21:15:56 -0500

    Initial Commit

---
+++ Drunk Weather/Drunk_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drunk_WeatherApp.swift
+//  Drunk Weather
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct Drunk_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
